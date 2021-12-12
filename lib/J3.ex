60cd7ba1249e5d4ea9bf9cd7ddf342195cf64828
lud <ludovic@demblans.com>
2021-12-12 19:10:00 +0100

    dynamic binding for names

--- lib/J3.ex
+++ lib/J3.ex
@@ -3,6 +3,35 @@ defmodule J3 do
   @pos_mid_left 2
   @pos_far_right 5
 
+  @lady_1 ~s|Natsiou|
+  @color_1 ~s|rouge|
+  @lady_2 ~s|Marcolla|
+  @color_2 ~s|vert|
+  @color_3 ~s|violet|
+  @color_4 ~s|blanc|
+  @drink_1 ~s|absinthe|
+  @city_1 ~s|Karnaca|
+  @color_5 ~s|bleu|
+  @object_1 ~s|bague|
+  @lady_3 ~s|Winslow|
+  @object_2 ~s|medaille|
+  @city_2 ~s|Baleton|
+  @object_3 ~s|diamant|
+  @object_4 ~s|pendentif|
+  @city_3 ~s|Dunwall|
+  @drink_2 ~s|bière|
+  @lady_4 ~s|Contee|
+  @drink_3 ~s|rhum|
+  @city_4 ~s|Fraeport|
+  @drink_4 ~s|whisky|
+  @drink_5 ~s|vin|
+  @lady_5 ~s|Finch|
+  @city_5 ~s|Dabokva|
+  @object_5 hd(
+              ~w(tabatiere diamant pendentif bague medaille) --
+                [@object_1, @object_2, @object_3, @object_4]
+            )
+
   defmodule Person do
     defstruct _n: nil,
               name: nil,
@@ -15,12 +44,12 @@ defmodule J3 do
 
   defguard is_value(v) when is_integer(v) or is_binary(v)
 
-  def names, do: ~w(Natsiou Winslow Marcolla Contee Finch)
+  def names, do: [@lady_1, @lady_2, @lady_3, @lady_4, @lady_5]
   def positions, do: @pos_far_left..@pos_far_right
-  def colors, do: ~w(rouge violet blanc vert bleu)
-  def objects, do: ~w(tabatiere diamant pendentif bague medaille)
-  def cities, do: ~w(Dunwall Karnaca Dabokva Baleton Fraeport)
-  def drinks, do: ~w(bière vin rhum whisky absinthe)
+  def colors, do: [@color_1, @color_2, @color_3, @color_4, @color_5]
+  def objects, do: [@object_1, @object_2, @object_3, @object_4, @object_5]
+  def cities, do: [@city_1, @city_2, @city_3, @city_4, @city_5]
+  def drinks, do: [@drink_1, @drink_2, @drink_3, @drink_4, @drink_5]
 
   # express that only people that have value_1 can have value_2.
   # - remove value_1 from people that do not have value_2
@@ -150,104 +179,104 @@ defmodule J3 do
     [
       # Les femmes s'assirent en rang. Elles étaient toutes vêtues de couleurs
       # différentes, et madame Natsiou portait un élégant chapeau rouge.
-      bind(:name, "Natsiou", :color, "rouge"),
+      bind(:name, @lady_1, :color, @color_1),
 
       # À l'extrême gauche se trouvait le docteur Marcolla, à côté de la convive au
       # veston vert.
-      bind(:name, "Marcolla", :position, @pos_far_left),
-      bind(:position, @pos_mid_left, :color, "vert"),
+      bind(:name, @lady_2, :position, @pos_far_left),
+      bind(:position, @pos_mid_left, :color, @color_2),
 
       # La dame en violet était assise à la gauche d'une personne en blanc. Je me
       # souviens de son habit violet, car elle l'avait taché d'absinthe.
-      remove(:position, 1, :color, "violet"),
-      remove(:position, 5, :color, "violet"),
-      remove(:position, 1, :color, "blanc"),
-      remove(:position, 3, :color, "blanc"),
-      limit(:color, ["violet", "blanc"], :position, 4),
-      bind(:color, "violet", :drink, "absinthe"),
+      remove(:position, 1, :color, @color_3),
+      remove(:position, 5, :color, @color_3),
+      remove(:position, 1, :color, @color_4),
+      remove(:position, 3, :color, @color_4),
+      limit(:color, [@color_3, @color_4], :position, 4),
+      bind(:color, @color_3, :drink, @drink_1),
       derive(
         fn people ->
-          Enum.filter(people, &has_choice?(&1, :color, "blanc"))
+          Enum.filter(people, &has_choice?(&1, :color, @color_4))
           |> pluck(:position)
           |> unique()
           |> left_of()
         end,
-        fn violet_poses -> limit(:position, violet_poses, :color, "violet") end
+        fn violet_poses -> limit(:position, violet_poses, :color, @color_3) end
       ),
       derive(
         fn people ->
-          Enum.filter(people, &has_choice?(&1, :color, "violet"))
+          Enum.filter(people, &has_choice?(&1, :color, @color_3))
           |> pluck(:position)
           |> unique()
           |> right_of()
         end,
-        fn blanc_poses -> limit(:position, blanc_poses, :color, "blanc") end
+        fn blanc_poses -> limit(:position, blanc_poses, :color, @color_4) end
       ),
 
       # La voyageuse venue de Karnaca portaint un ensemble bleu. Quand l'une des
       # invitées se vanta d'avoir une bague en sa possession, sa voisine déclara qu'on
       # trouvait largement mieux à Karnaca, où elle habitait.
-      bind(:city, "Karnaca", :color, "bleu"),
+      bind(:city, @city_1, :color, @color_5),
       derive(
         fn people ->
-          Enum.filter(people, &has_choice?(&1, :city, "Karnaca"))
+          Enum.filter(people, &has_choice?(&1, :city, @city_1))
           |> pluck(:position)
           |> unique()
           |> neighbours_of()
         end,
         fn
           [bague_pos] ->
-            bind(:position, bague_pos, :object, "bague")
+            bind(:position, bague_pos, :object, @object_1)
 
           bague_poses ->
             bague_poses |> IO.inspect(label: ~S[bague_poses])
-            limit(:position, bague_poses, :object, "bague")
+            limit(:position, bague_poses, :object, @object_1)
         end
       ),
       derive(
         fn people ->
-          Enum.filter(people, &has_choice?(&1, :object, "bague"))
+          Enum.filter(people, &has_choice?(&1, :object, @object_1))
           |> pluck(:position)
           |> unique()
           |> neighbours_of()
         end,
-        fn karnaca_poses -> limit(:position, karnaca_poses, :city, "Karnaca") end
+        fn karnaca_poses -> limit(:position, karnaca_poses, :city, @city_1) end
       ),
 
       # Alors, dame Winslow exhiba fièrement une médaille de guerre de grande valeur,
       # dont la dame de Baleton s'empressa de se moquer en montrant à son tour un
       # diamant qui, selon elle, était bien plus remarquable.
-      bind(:name, "Winslow", :object, "medaille"),
-      bind(:city, "Baleton", :object, "diamant"),
+      bind(:name, @lady_3, :object, @object_2),
+      bind(:city, @city_2, :object, @object_3),
 
       # Une autre femme avait apporté un pendentif maginifique ; quand la visiteuse de
       # Dunwall à côté d'elle l'aperçut, elle faillit renverser le verre de bière de sa
       # voisine.
-      bind(:object, "pendentif", :drink, "bière"),
-      remove(:city, "Dunwall", :object, "pendentif"),
-      remove(:city, "Dunwall", :drink, "bière"),
+      bind(:object, @object_4, :drink, @drink_2),
+      remove(:city, @city_3, :object, @object_4),
+      remove(:city, @city_3, :drink, @drink_2),
       derive(
         fn people ->
-          Enum.filter(people, &has_choice?(&1, :object, "pendentif"))
+          Enum.filter(people, &has_choice?(&1, :object, @object_4))
           |> pluck(:position)
           |> unique()
           |> neighbours_of()
         end,
-        fn dunwall_poses -> limit(:position, dunwall_poses, :city, "Dunwall") end
+        fn dunwall_poses -> limit(:position, dunwall_poses, :city, @city_3) end
       ),
 
       # Ensuite, la comtesse Contee se resservit un peut de rhum pour porter un toast.
-      bind(:name, "Contee", :drink, "rhum"),
+      bind(:name, @lady_4, :drink, @drink_3),
 
       # La dame de Fraeport, grisée de whisky, sauta sur la table et tomba sur la
       # convive assise au centre, renversant le verre de vin de la pauvre femme.
-      bind(:city, "Fraeport", :drink, "whisky"),
-      remove(:city, "Fraeport", :position, 3),
-      bind(:drink, "vin", :position, 3),
+      bind(:city, @city_4, :drink, @drink_4),
+      remove(:city, @city_4, :position, 3),
+      bind(:drink, @drink_5, :position, 3),
 
       # Plus tard, la baronne Finch captiva l'assemblée en racontant une histoire de sa
       # folle jeunesse à Dabokva.
-      bind(:name, "Finch", :city, "Dabokva"),
+      bind(:name, @lady_5, :city, @city_5),
 
       # Au matin, il y avait quatre objets de valeur sous la table : une bague, une
       # tabatière, une diamant et un pendentif.
@@ -271,7 +300,16 @@ defmodule J3 do
         }
       end
 
-    rules() |> Stream.cycle() |> Stream.scan(people, &apply_rule/2) |> Stream.run()
+    rules()
+    |> Stream.cycle()
+    |> Stream.scan(people, &apply_rule/2)
+    |> Enum.reduce_while(nil, fn people, _ ->
+      if win?(people) do
+        {:halt, people}
+      else
+        {:cont, nil}
+      end
+    end)
   end
 
   defp apply_rule(:noop, people) do
@@ -280,15 +318,6 @@ defmodule J3 do
 
   defp apply_rule(rule, people) do
     new_people = rule.(people) |> reduce_choices() |> Enum.sort_by(&{&1.position, &1._n})
-
-    if win?(new_people) do
-      new_people
-      |> IO.inspect(label: ~S[new_people])
-
-      System.halt()
-    else
-      new_people
-    end
   end
 
   # scan all people. if people have a single value for a field, ensure that
--- tt.exs
+++ tt.exs
@@ -1,1 +1,4 @@
 J3.run()
+|> Enum.each(fn %J3.Person{_n: name, object: [object]} ->
+  IO.puts("#{name}: #{object}")
+end)
